b74328ad11df554880cdc531332f9ea8f9fe23c3
python_dev <64308925+Omar-Rasheed786@users.noreply.github.com>
2021-07-22 21:04:02 +0500

    Update settings.py

--- report_project/settings.py
+++ report_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nua$27$isjnq)#^5g0+vvjnua=++tfmrwr$c7bbw9o*v@1dh+s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['outbound1predictor.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
